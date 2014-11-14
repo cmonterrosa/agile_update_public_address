20e1b185aceb884a00d90859bcbe59d32276679a
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-11-14 13:13:07 -0600

    + Add method to update timestamp

--- server/app/models/host.rb
+++ server/app/models/host.rb
@@ -1,8 +1,9 @@
 class Host < ActiveRecord::Base
 before_save :update_datetime
+before_update :update_datetime
 
  def update_datetime
-   self.updated_at ||= Time.now
+   self.updated_at = Time.now
  end
 
 end
